cc2960179bb36145afbbc62c363c0b0602215590
Kevin Wang <javaperl@gmail.com>
2019-01-23 18:50:32 -0800

    applied patch: st-iso14755-20180911-67d0cb6.diff

--- config.def.h
+++ config.def.h
@@ -177,6 +177,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 };
 
 /*
