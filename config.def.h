76469136b95124d56a5523c04e3539c2868f30ea
Kevin Wang <javaperl@gmail.com>
2019-01-28 11:34:42 -0800

    added Fira Mono font

--- config.def.h
+++ config.def.h
@@ -6,6 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* static char *font = "Fira Mono:pixelsize=16:antialias=true:autohint=true"; */
 static int borderpx = 2;
 
 /*
